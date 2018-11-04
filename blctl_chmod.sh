767ed7fb90aada089ffa962bab40503b6bd33895
fanhui <hui.fan@mail.ru>
2018-11-04 15:19:38 +0800

    =-=

--- blctl_chmod.sh
+++ blctl_chmod.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 setpci -v -H1 -s 00:01.00 BRIDGE_CONTROL=0
-chmod a+w /sys/class/backlight/intel_backlight/brightness
+
+path=/sys/class/backlight/intel_backlight/brightness
+
+echo 600 > $path
+chmod a+w $path
